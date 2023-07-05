6ac327d463978569e3468d8aa9bd96ce3205afbd
APerlinx <133282938+APerlinx@users.noreply.github.com>
2023-07-05 15:31:08 +0300

    Update

--- Sprint3-Appsus/apps/keep/cmps/NotePreview.js
+++ Sprint3-Appsus/apps/keep/cmps/NotePreview.js
@@ -1,14 +1,13 @@
 export default {
     props: ['note'],
     template: `
-        <article class="note-preview">
-            <h1>{{note.info.title}}</h1>
-            <p>Txt</p>
-        </article>
-             <section class="links">
-            <!-- <RouterLink :to="'/note/' + note.id" class="details-link">Details</RouterLink>
-            <RouterLink :to="'/note/edit/' + note.id" class="edit-link">Edit</RouterLink> -->
-            </section>
+       <article class="note-preview">
+    <component :is="getComponentName(note.type)" :note="note"></component>
+    <section class="links">
+      <!-- <RouterLink :to="'/note/' + note.id" class="details-link">Details</RouterLink>
+      <RouterLink :to="'/note/edit/' + note.id" class="edit-link">Edit</RouterLink> -->
+    </section>
+  </article>
     `,
     computed: {
     }
