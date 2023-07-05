f0aecf3c3ef93050a89760ef76ddb783d39aa480
APerlinx <133282938+APerlinx@users.noreply.github.com>
2023-07-05 18:18:06 +0300

    Add tool tip and side-bar

---
+++ apps/keep/cmps/KeepMenu.js
@@ -0,0 +1,15 @@
+import { noteService } from '../services/note.service.js';
+
+export default {
+  template: `  
+         <section class="keep-app-menu">
+         <i class="mdi mdi-lightbulb-on-outline"></i> 
+         <i class="mdi mdi-bell-outline"></i> 
+         <i class="mdi mdi-pencil"></i> 
+         <i class="mdi mdi-archive"></i> 
+         <i class="mdi mdi-trash-can-outline"></i> 
+        </section>  
+       
+    `,
+  computed: {},
+};
--- apps/keep/cmps/NoteTxt.js
+++ apps/keep/cmps/NoteTxt.js
@@ -1,15 +1,12 @@
 export default {
     props: ['note'],
     template: `
-        <article class="note-preview">
-            <pre>{{note.info.txt}}</pre>
-            <p>txt</p>
+        <article class="note-txt">
+        <h2>{{ note.info.title }}</h2>
+            <p>{{ note.info.txt }}</p>
         </article>
-             <section class="links">
-            <!-- <RouterLink :to="'/note/' + note.id" class="details-link">Details</RouterLink>
-            <RouterLink :to="'/note/edit/' + note.id" class="edit-link">Edit</RouterLink> -->
-            </section>
     `,
     computed: {
+        
     }
 }
