a58b0c9ebc0e67d4b5711e7c6d10728296556fc8
APerlinx <133282938+APerlinx@users.noreply.github.com>
2023-07-05 12:53:50 +0300

    Create design skeleton and some notes

--- Sprint3-Appsus/apps/keep/pages/NoteIndex.js
+++ Sprint3-Appsus/apps/keep/pages/NoteIndex.js
@@ -1,5 +1,23 @@
 export default {
 	template: `
-    <h1>Notes!</h1>
+    <main class="keep-app">
+       <div class="add-note" contenteditable="true" role="textbox">
+       <button class="add-icon" title="Add Note"><i class="fas fa-plus"></i></button>
+       <button class="delete-icon" title="Delete Note"><i class="fas fa-trash"></i></button>
+       </div>  
+        <section class="notes">
+          
+          <div class="note">im a note</div>
+          <div class="note">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+          <div class="note">Eius vero, fugiat nobis excepturi doloremque laboriosam</div>
+          <div class="note">id quam dolor, dolorem repellat distinctio accusantium ad iusto earum nesciunt similique tempore deserunt? In?</div>
+          <div class="note">Eius vero, fugiat nobis excepturi doloremque laboriosam</div>
+          <div class="note">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+        </section>
+        <nav class="menu">
+            <h1>navbar</h1>
+        </nav>
+    </main>
+  
     `,
 }
---
+++ Sprint3-Appsus/apps/keep/services/note.service.js
@@ -0,0 +1,169 @@
+import { utilService } from '../../../services/util.service.js';
+import { storageService } from '../../../services/async-storage.service.js'
+
+// import notesData from '../data/note-data.js'
+
+const PAGE_SIZE = 5;
+const NOTE_KEY = 'noteDB';
+
+var gFilterBy = {  };
+var gSortBy = { };
+var gPageIdx;
+
+_createNote()
+
+export const noteService = {
+  query,
+  get,
+  remove,
+  save,
+  getEmptynote,
+  getNextnoteId,
+  getFilterBy,
+  setFilterBy,
+  getnoteCountBySpeedMap,
+  addReview,
+  removeReview,
+};
+window.noteService = noteService;
+
+function query() {
+  return storageService.query(NOTE_KEY).then((notes) => {
+//     if (gFilterBy.title) {
+//       const regex = new RegExp(gFilterBy.title, 'i');
+//       notes = notes.filter((note) => regex.test(note.title));
+//     }
+//     if (gPageIdx !== undefined) {
+//       const startIdx = gPageIdx * PAGE_SIZE;
+//       notes = notes.slice(startIdx, startIdx + PAGE_SIZE);
+//     }
+//     if (gSortBy.price !== undefined) {
+//         notes.sort((c1, c2) => (c1.price - c2.price) * gSortBy.price);
+//     } else if (gSortBy.title !== undefined) {
+//         notes.sort((c1, c2) => c1.title.localeCompare(c2.title) * gSortBy.title);
+//     }
+
+//     return notes;
+//   });
+}
+
+function get(noteId) {
+  return storageService.get(NOTE_KEY, noteId)
+      .then(note => _setNextPrevNoteId(note))
+}
+
+function remove(noteId) {
+  return storageService.remove(NOTE_KEY, noteId);
+}
+
+function save(note) {
+  if (note.id) {
+    return storageService.put(NOTE_KEY, note);
+  } else {
+    return storageService.post(NOTE_KEY, note);
+  }
+}
+
+function getEmptynote() {
+ 
+}
+
+function getFilterBy() {
+  return { ...gFilterBy };
+}
+
+function setFilterBy(filterBy = {}) {
+  if (filterBy.title !== undefined) gFilterBy.title = filterBy.title;
+  if (filterBy.price !== undefined) gFilterBy.price = filterBy.price;
+  return gFilterBy;
+}
+
+function getNextnoteId(noteId) {
+  return storageService.query(NOTE_KEY).then((notes) => {
+    var idx = notes.findIndex((note) => note.id === noteId);
+    if (idx === notes.length - 1) idx = -1;
+    return notes[idx + 1].id;
+  });
+}
+
+function getnoteCountBySpeedMap() {
+  return storageService.query(NOTE_KEY).then((notes) => {
+    const noteCountBySpeedMap = notes.reduce(
+      (map, note) => {
+        if (note.price < 120) map.cheap++;
+        else if (note.price < 200) map.normal++;
+        else map.expensive++;
+        return map;
+      },
+      { cheap: 0, normal: 0, expensive: 0 }
+    );
+    return noteCountBySpeedMap;
+  });
+}
+
+
+function addReview(noteId, review) {
+  return get(noteId)
+      .then(note => {
+          if (!note.reviews) note.reviews = []
+          review.id = utilService.makeId()
+          note.reviews.push(review)
+          return save(note)
+      })
+}
+
+function removeReview(noteId, reviewId) {
+  return get(noteId)
+      .then(note => {
+          const idx = note.reviews.findIndex(review => review.id === reviewId)
+          note.reviews.splice(idx, 1)
+          return save(note)
+      })
+}
+
+function _setNextPrevnoteId(note) {
+  return storageService.query(NOTE_KEY)
+      .then(notes => {
+          const noteIdx = notes.findIndex(currNote => currNote.id === note.id)
+          note.nextnoteId = notes[noteIdx + 1] ? notes[noteIdx + 1].id : notes[0].id
+          note.prevnoteId = notes[noteIdx - 1]
+              ? notes[noteIdx - 1].id
+              : notes[notes.length - 1].id
+          return note
+      })
+}
+
+function _createnotes() {
+  let notes = utilService.loadFromStorage(NOTE_KEY);
+  if (!notes || !notes.length) {
+    notes = notesData;
+    utilService.saveToStorage(note_KEY, notes);
+  }
+}
+
+function _createnote(title) {
+  const note = getEmptynote(title);
+  note.id = utilService.makeId();
+  return note;
+}
+
+function _createDemoNotes() {
+    const noteTitle = ['note1', 'note2', 'note3']
+    const noteDescs = ['placerat nisi sodales suscipit tellus', 'placerat sodales suscipit tellus', 'placerat nisi suscipit']
+    const thumbnail = 'http://ca.org/notes-photos/20.jpg'
+    const listPrice = [
+    {amount:10,currencyCode:'EUR',isOnSale:false},
+    {amount:15,currencyCode:'EUR',isOnSale:false},
+    {amount:20,currencyCode:'EUR',isOnSale:false}
+]
+    
+    const notes = noteTitle.map((noteTitle, i) => {
+        const note = _createnote(noteTitle)
+        note.desc = noteDescs[i]
+        note.thumbnail = thumbnail[i]
+        note.listPrice = listPrice[i]
+        return note
+    })
+
+    utilService.saveToStorage(NOTES_KEY, notes)
+}
